07f32411802e81f8c748f999446c0e3d484b11a5
comatrix <oboists_cloud@aleeas.com>
2022-07-18 16:15:58 +0800

    changed browserRouter to hashRouter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import "./App.css";
 import Header from "./components/Header";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Switch,
-} from "react-router-dom";
+import { HashRouter, Route, Routes, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import Cart from "./components/Cart";
 import Layout from "./components/Layout";
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="cart" element={<Cart />} />
         </Route>
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
